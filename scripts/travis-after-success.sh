3b616f24da6b37fda7547295cfb3a90425de7376
Niklas Claesson <nicke.claesson@gmail.com>
2017-10-13 11:12:11 +0200

    wip

---
+++ scripts/travis-after-success.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# Exit on error
+set -e
+# Print each line
+set -v
+
+if "${TRAVIS_EVENT_TYPE}" == "cron"; then
+	./scripts/deploy_doxygen
+fi
---
+++ scripts/travis-before-script.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# Exit on error
+set -e
+# Print each line
+set -v
+
+if "${TRAVIS_EVENT_TYPE}" == "cron"; then
+	sudo apt-get update -qq
+	sudo apt-get install -y doxygen doxygen-doc doxygen-latex doxygen-gui graphviz
+fi
